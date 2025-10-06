a0af66f27546692ffb4e8d489e7c2f7cf8206f46
GACoutts <ST10258297@imconnect.edu.za>
2025-10-06 15:47:14 +0200

    Fixed little errors, Connected to firebase, Need to re-make sign-in page

--- app/src/main/java/com/example/mixmate/HomePage.kt
+++ app/src/main/java/com/example/mixmate/HomePage.kt
@@ -3,11 +3,7 @@ package com.example.mixmate
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 
 class HomePage : AppCompatActivity() {
 
@@ -15,13 +11,7 @@ class HomePage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
 
         //btnLogOut = findViewById(R.id.btnL)
 
