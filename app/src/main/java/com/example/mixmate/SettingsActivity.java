7ff411b68a467ffff67358c2231117f11a4d8e6f
GACoutts <ST10258297@imconnect.edu.za>
2025-10-07 15:35:52 +0200

    Trying to get home page working and looking as it should

--- app/src/main/java/com/example/mixmate/SettingsActivity.java
+++ app/src/main/java/com/example/mixmate/SettingsActivity.java
@@ -167,6 +167,7 @@ public class SettingsActivity extends AppCompatActivity {
         cardLogout.setOnClickListener(v -> {
             Log.d(TAG, "Logout clicked");
             showLogoutDialog();
+
         });
     }
 
