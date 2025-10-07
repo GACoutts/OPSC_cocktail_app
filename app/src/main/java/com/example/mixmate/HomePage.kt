dc7f709d767966d1c8b137704517dd7e626aa8ab
GACoutts <ST10258297@imconnect.edu.za>
2025-10-07 18:21:37 +0200

    Stunted homepage so that the app doenst crash Working Sign in and Sign up

--- app/src/main/java/com/example/mixmate/HomePage.kt
+++ app/src/main/java/com/example/mixmate/HomePage.kt
@@ -13,28 +13,8 @@ class HomePage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-    //menu inflation
-    fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu) // replace with your menu file name
-        return true
-    }
+        //menu inflation
 
-    // Handle toolbar item clicks
-    fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.btnLogOut -> {
-                // Handle logout
-                val intent = Intent(this, DiscoverPage::class.java)
-                startActivity(intent)
-                finish()
-                true
-            }
-            R.id.action_search -> {
-                // Handle search
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
+    }
 }
